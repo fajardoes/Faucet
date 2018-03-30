f2dfae079b26521f2ccd047aef795da2a50efde7
Daniel Fajardo <37449584+fajardoes@users.noreply.github.com>
2018-03-30 10:35:49 -0600

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 // Connection variables 
 $host = "localhost"; // MySQL host name eg. localhost
 $user = "root"; // MySQL user. eg. root ( if your on localserver)
-$password = ""; // MySQL user password  (if password is not set for your root user then keep it empty )
+$password = "MrRobot.2018"; // MySQL user password  (if password is not set for your root user then keep it empty )
 $database = "faucet"; // MySQL Database name
  
 // Connect to MySQL Database
